146a27befbb89fbe0df444ff1b8e4481c514c0b3
chelseazheng <chelseazheng826@gmail.com>
2019-01-08 11:53:40 -0500

    Updated build file

--- buildapk.sh
+++ buildapk.sh
@@ -1,3 +1,7 @@
+npm i
+
+react-native link
+
 react-native bundle --dev false --platform android --entry-file index.js --bundle-output ./android/app/build/intermediates/assets/debug/index.android.bundle --assets-dest ./android/app/build/intermediates/res/merged/debug
 
 cd android
